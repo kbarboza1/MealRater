977166993f99e14051b556246d7b69002ccaf8e7
kimberlybarboza01 <95250937+kimberlybarboza01@users.noreply.github.com>
2022-04-18 23:55:51 -0400

    database

--- MealRater/ViewController.swift
+++ MealRater/ViewController.swift
@@ -6,15 +6,46 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var txtResturant: UITextField!
+    @IBOutlet weak var txtDish: UITextField!
     @IBOutlet weak var Lable: UILabel!
+    
+    var currentMeal: Meal?
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if currentMeal == nil {
+            let context = appDelegate.persistentContainer.viewContext
+            currentMeal = Meal(context: context)
+        }
+        currentMeal?.resturant = txtResturant.text
+        currentMeal?.dish = txtDish.text
+        return true
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         NotificationCenter.default.addObserver(self, selector: #selector(didGetRating(_:)), name: Notification.Name("rate"), object: nil)
+        
         // Do any additional setup after loading the view.
+        
+        //updated currentMeal with the values entered
+        
+        //textFields listeners used to save data
+        let textFields:[UITextField] = [txtResturant, txtDish]
+        
+        for textField in textFields{
+            textField.addTarget(self, action: #selector(UITextFieldDelegate.textFieldShouldEndEditing(_:)),
+                                for: UIControl.Event.editingDidEnd)
+        }
+    }
+    @objc func saveMeal() {
+        appDelegate.saveContext()
+        
     }
     @objc func didGetRating(_ notification: Notification){
             let rate = notification.object as! String?
@@ -25,7 +56,12 @@ class ViewController: UIViewController {
         let vc = storyboard?.instantiateViewController(withIdentifier: "rateView") as! RateViewController
         vc.modalPresentationStyle = .fullScreen
         present(vc, animated: true)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .save,
+                                                            target: self,
+                                                            action: #selector(self.saveMeal))
     }
+    
+    
 
     
 
